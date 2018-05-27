dfc4eb0628229924d5dddb45216def19e8c79ea1
Omar-EB <omar.elboraey99@gmail.com>
2018-05-27 15:39:45 -0400

    added settings to toolbar (with menu file and  methods) and adjusted spinner

--- app/src/main/java/com/venom/tipcalculator/MainActivity.java
+++ app/src/main/java/com/venom/tipcalculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.venom.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 //import com.venom.tipcalculator.R;
@@ -27,5 +30,25 @@ public class MainActivity extends AppCompatActivity {
                 list);
         numberOfPPL.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         numberOfPPL1.setAdapter(numberOfPPL);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings:
+               // start settings activity
+                return true;
+            default:
+        }
+        return false;
     }
 }
